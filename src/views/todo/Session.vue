0e73e6904dc2cca3832c3b83b3085f1ac79e3256
Elon <Elon@DESKTOP-I8F345R>
2019-09-17 23:11:51 +0800

    Update Session.vue
    update session

--- src/views/todo/Session.vue
+++ src/views/todo/Session.vue
@@ -1,17 +1,19 @@
 <template>
 
-            <el-collapse>
-<el-collapse-item>
+            <el-collapse style="
+  border: 0px solid;">
+<el-collapse-item class="title">
     <template slot="title">
-      一致性 Consistency<sapn style="margin-left:700px">asdf</sapn>
+      一致性 Consistency
+      <sapn style="margin-left:70%">asdf</sapn>
     </template>
             <el-row :gutter="50">
-                <el-col :span="8" style="background-color: #d3dce6" bg-purple><div class="grid-content bg-purple">startTime: {{session.startTime}}</div></el-col>
+                <el-col :span="8" style="background-color: #d3dce6;text-align:center" bg-purple><div class="grid-content bg-purple">startTime: {{session.startTime}}</div></el-col>
                 <el-col :span="8" style="background-color: #e5e9f2"><div class="grid-content bg-purple">endTime: {{session.endTime}}</div></el-col>
-                <el-col :span="8"><div class="grid-content bg-purple">durantion:{{session.durantion}}</div></el-col>
+                <el-col :span="8"><div class="grid-content bg-purple">durantion:{{durantion(session)}}</div></el-col>
                 <el-col :span="8"><div class="grid-content bg-purple">startProgress: {{session.startProgress}}</div></el-col>
                 <el-col :span="8"><div class="grid-content bg-purple">endProgress: {{session.endProgress}}</div></el-col>
-                <el-col :span="8"><div class="grid-content bg-purple">finishProgress: {{session.finishProgress}}</div></el-col>
+                <el-col :span="8"><div class="grid-content bg-purple" v-model="finishProgress">finishProgress: {{finishProgress}}</div></el-col>
             </el-row>
 </el-collapse-item>
 
@@ -31,16 +33,32 @@ export default {
                     durantion: 20,
                     startProgress:0,
                     endProgress:20,
-                    finishProgress:20
+                    // finishProgress:20
                 }
         }
     },
     computed:{
-        
+        durantion() {
+        return function(session) {
+            console.log(session)
+          return session.endTime- session.startTime
+        }
+      },
+
+                finishProgress: function () {
+                return this.session.endProgress - this.session.startProgress
+                }
+               
     }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.title {
+  width: 50%;
+  margin: 0 auto;
+  border: 1px inset;
+  border-radius:30px 50px 0 0;
+  background-color: #aaf;
+}
 </style>
