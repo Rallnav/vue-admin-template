3ed7ebedc1a55c8bea216b2a8495e884365b75d0
Elon <Elon@126.com>
2019-09-19 21:00:31 +0800

    Update Session.vue
    新增弹框编辑

--- src/views/todo/Session.vue
+++ src/views/todo/Session.vue
@@ -6,58 +6,49 @@
     <template slot="title" style="height:auto">
         <span style="margin-left:5%">{{session.startTime}}</span>
     </template>
-            <el-row :gutter="50">
-                <el-col class="light-info" :span="8"><div class="grid-content bg-purple">开始时间: {{session.startTime}}</div></el-col>
-                <el-col class="dark-info" :span="8" ><div class="grid-content bg-purple">结束时间: {{session.endTime}}</div></el-col>
-                <el-col class="light-info" :span="8"><div class="grid-content bg-purple">持续时间:{{durantion}}</div></el-col>
-                <el-col class="dark-info" :span="8"><div class="grid-content bg-purple">起始进度: {{session.startProgress}}</div></el-col>
-                <el-col class="light-info" :span="8"><div class="grid-content bg-purple">结束进度: {{session.endProgress}}</div></el-col>
-                <el-col class="dark-info" :span="8"><div class="grid-content bg-purple">完成进度: {{finishProgress}}</div></el-col>
-            </el-row>
-    <el-button v-on:click="handleEdit()">
+            <el-row :gutter="50" class="container">
+                <el-col class="light-info" :span="8" ><div class="grid-content bg-purple" > 开始时间: {{session.startTime}}</div></el-col>
+                <el-col class="dark-info" :span="8" ><div class="grid-content bg-purple" > 结束时间: {{session.endTime}}</div></el-col>
+                <el-col class="light-info" :span="8" ><div class="grid-content bg-purple" > 持续时间: {{duration}}</div></el-col>
+
+                </el-row>
+                <el-row :gutter="50" type="flex">
+                <el-col class="dark-info" :span="8"><div class="grid-content bg-purple" >起始进度: {{session.startProgress}}</div></el-col>
+                <el-col class="light-info" :span="8"><div class="grid-content bg-purple" >结束进度: {{session.endProgress}}</div></el-col>
+                <el-col class="dark-info" :span="8"><div class="grid-content bg-purple" >完成进度: {{finishProgress}}</div></el-col>
+                 </el-row>
+                <el-row :gutter="50" type="flex">
+                <el-col class="light-info" :span="8"><div class="grid-content bg-purple" >完成速度: {{speed}}</div></el-col>
+                <el-col class="dark-info" :span="8"><div class="grid-content bg-purple" >完成效率: {{effecient}}</div></el-col>
+                <el-col class="light-info" :span="8"><el-button v-on:click="handleEdit()">
     edit
-    </el-button>
+    </el-button></el-col>
+            </el-row>
+    
 </el-collapse-item>
 
-<!-- <el-dialog class="edit-dialog" width="80%" style="top:-12%;" append-to-body @close="reset()"
+<el-dialog class="edit-dialog" width="80%" style="top:-12%;" append-to-body @close="reset()"
                :title="editDialogDisplayTitle"
                :visible.sync="editDialogVisible">
       <el-form status-icon label-width="125px"
-               :model="editForm" :rules="editFormRules" ref="editForm">
+               :model="editSession" :rules="rules" ref="editForm">
         <el-row style="width:100%;">
           <el-col style="width:40%;">
             <el-form-item prop="id">
-              <el-input v-model="editForm.id" type="hidden"></el-input>
+              <el-input v-model="editSession.id" type="hidden"></el-input>
             </el-form-item>
-            <el-form-item label="用户名：" prop="username">
-              <el-input v-model="editForm.username" :disabled="usernameDisable"></el-input>
+            <el-form-item label="开始时间：" prop="startTime">
+              <el-input v-model="editSession.startTime" :disabled="disable"></el-input>
             </el-form-item>
-            <el-form-item label="姓名：" prop="name">
-              <el-input v-model="editForm.name"></el-input>
+            <el-form-item label="结束时间：" prop="endTime">
+              <el-input v-model="editSession.endTime"></el-input>
             </el-form-item>
-            <el-form-item label="密码：" prop="password">
-              <el-input type="password" v-model="editForm.password"></el-input>
+            <el-form-item label="起始进度：" prop="startProgress">
+              <el-input  v-model="editSession.startProgress"></el-input>
             </el-form-item>
-            <el-form-item label="确认密码：" prop="checkPassword">
-              <el-input type="password" v-model="editForm.checkPassword"></el-input>
+            <el-form-item label="完成进度：" prop="endProgress">
+              <el-input  v-model="editSession.endProgress"></el-input>
             </el-form-item>
-            <el-form-item label="邮箱：" prop="email">
-              <el-input v-model="editForm.email"></el-input>
-            </el-form-item>
-            <el-form-item label="手机号：" prop="phone">
-              <el-input v-model="editForm.phone"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col style="width:60%;float:left;">
-            <div style="margin-left:5%;">
-              <template>
-                <div class="grid-content bg-purple" style="margin-left:5%;font-style: oblique;color:blue;margin:5px;">
-                  设置角色
-                </div>
-                <el-transfer style="height:200px;" v-model="editForm.selrolearr" :data="roleData"
-                             :titles="['未选角色','已选角色']"></el-transfer>
-              </template>
-            </div>
           </el-col>
         </el-row>
 
@@ -66,19 +57,30 @@
         <el-button type="info" @click="editDialogVisible = false">取消</el-button>
         <el-button type="primary" :loading="editBtnLoading" @click="editSave()">保存</el-button>
       </span>
-    </el-dialog> -->
+    </el-dialog>
 
 </el-collapse>
 </template>
 
 <script>
 export default {
-    props: ['initSession'],
+    // props: ['initSession'],
     data(){
         return{
-            editDialogDisplayTitle:false,
-            session:this.initSession,
+            editDialogDisplayTitle:"edit",
+            editDialogVisible:false,
+            editBtnLoading:false,
+            // session:this.initSession,
             session:{
+                    description:"一致性 Consistency如果这是一个tetttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
+                    id:1,
+                    parent:"",
+                    startTime: new Date(),
+                    endTime:new Date(),
+                    startProgress:0,
+                    endProgress:20,
+                },
+            editSession:{
                     description:"一致性 Consistency如果这是一个tetttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
                     id:1,
                     parent:"",
@@ -86,31 +88,49 @@ export default {
                     endTime:new Date().toLocaleString(),
                     startProgress:0,
                     endProgress:20,
-                }
+                },
+            disable:false,
+            rules:{
+            }   
         }
     },
     computed:{
-        durantion: function () {
+        duration: function () {
             return this.session.endTime - this.session.startTime
         },
         finishProgress: function () {
             return this.session.endProgress - this.session.startProgress
+        },
+        speed:function(){
+            return this.duration/this.finishProgress
+        },
+        effecient:function(){
+            return this.finishProgress/this.duration
         }
                
     },
     methods:{
-         handleEdit(scope) {
-      this.dialogType = 'edit'
-      this.dialogVisible = true
-      this.checkStrictly = true
-      this.role = deepClone(scope.row)
-      this.$nextTick(() => {
-        const routes = this.generateRoutes(this.role.routes)
-        this.$refs.tree.setCheckedNodes(this.generateArr(routes))
-        // set checked state of a node not affects its father and child nodes
-        this.checkStrictly = false
-      })
+        handleEdit() {
+        this.editDialogDisplayTitle = 'edit'
+        this.editDialogVisible = true
+        this.editSession=JSON.parse(JSON.stringify(this.session))
+      
     },
+    // 表单重置
+      reset() {
+        let editForm = this.$refs["editForm"];
+        editForm.clearValidate();
+        editForm.resetFields();
+        // @特殊处理, 针对编辑情况的重置
+        // for (let k in this.editForm) {
+        //   this.editForm[k] = '';
+        // }
+      },
+      editSave(){
+          this.session=JSON.parse(JSON.stringify(this.editSession))
+          this.$emit("changeData",this.session)
+          this.editDialogVisible = false
+      }
     }
 }
 </script>
@@ -127,12 +147,30 @@ export default {
 }
 
 .light-info{
-    background-color: #d3dce6;
-    text-align:center
+    align-items: stretch;
+    background-color: #e5e9f2;
+    text-align:center;
+    vertical-align:middle;
 }
 
 .dark-info{
+    background-color: #d3dce6;
+    text-align:center;
+    align-self:auto;
+}
+
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex:1 1 auto;
+  .light-info{
+    align-items: stretch;
     background-color: #e5e9f2;
-    text-align:center
+    text-align:center;
+    vertical-align:middle;
+}
+
+
 }
 </style>
